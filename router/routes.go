7d7f60439ef9c0c2f09cacb787d1797d15b57105
Biankowski <rodrigobianchini96@mgail.com>
2024-05-18 19:02:49 -0300

    feat: update handlers

--- router/routes.go
+++ router/routes.go
@@ -1,41 +1,17 @@
 package router
 
 import (
+	"github.com/Biankowski/go/handler"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Api com Handler",
-				"name":    "Rodrigo",
-				"age":     27,
-			})
-		})
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Api com Handler",
-			})
-		})
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Api com Handler",
-			})
-		})
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Api com Handler",
-				"name":    "Rodrigo",
-				"age":     27,
-			})
-		})
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "Api com Handler",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningHandler)
 	}
 }
