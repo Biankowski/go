cd1ea07d778d10fa48312db6a2f287ba29912510
Biankowski <rodrigobianchini96@mgail.com>
2024-05-18 22:37:47 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"github.com/Biankowski/go/config"
 	"github.com/Biankowski/go/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
